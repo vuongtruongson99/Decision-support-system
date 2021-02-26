7054670db1f0eb6b6e80887bf4ba37d96fe5bf48
Truong Son <tr.son.2112@gmail.com>
2021-02-26 20:45:27 +0700

    Add method in Pareto

--- Method Pareto/pareto.py
+++ Method Pareto/pareto.py
@@ -1,46 +1,20 @@
-pos_criterion_name = ['Screen', 'SSD', 'Pin', 'Rate']
-neg_criterion_name = ['Price', 'Weight']
-alternatives = [[['Solution'], ['Screen', 'SSD', 'Pin', 'Rate'], ['Price', 'Weight']], [['Acer Aspire 5 A515-44G'], ['15.6', '256', '10', '5'], ['60.9', '1.8']], [['ASUS ZenBook 13 UX325EA'], ['13.3', '512', '18', '5'], ['76.6', '1.07']], [['Lenovo IdeaPad 5 15'], ['15.6', '512', '14', '4.7'], ['68.9', '1.7']], [['Lenovo Legion Y540-15'], ['15.6', '128', '9', '4.6'], ['68.6', '2.3']], [['Xiaomi RedmiBook 16" Ryzen Edition'], ['16.1', '512', '13', '4.7'], ['61.9', '1.8']], [['HONOR MagicBook Pro'], ['16.1', '512', '11', '4.5'], ['56.8', '1.7']], [['ASUS VivoBook S15 M533IA'], ['15.6', '512', '6', '4.8'], ['57.5', '1.8']], [['Lenovo Yoga Slim 7 14'], ['14', '1024', '15', '4'], ['79.9', '1.4']], [['MSI GF63 Thin 9SCXR'], ['15.6', '512', '7', '4.6'], ['66.6', '1.9']], [['Acer Nitro 5 AN515-54'], ['15.6', '512', '8', '4.5'], ['59.8', '2.3']]]
+pos_criterion_name = []
+neg_criterion_name = []
+alternatives = []
 
+# Input alternative from file input.txt
 def input_alternative():
-    alternatives_name = []
-    print("Enter alternatives name: ")
-    while True:
-        name = input("\t")
-        if name:
-            alternatives_name.append(name)
-        else:
-            break
+    f = open("input.txt", "r")    
+    for line in f:
+        alter = []
+        alternative = line.strip("\n").split("|")
+        for criterion in alternative:
+            alter.append(criterion.split(","))
+        alternatives.append(alter)
 
-    pos_criterion_number = int(input("Enter number of positive criterion: "))
-    print("\tEnter " + str(pos_criterion_number) + " criterion name: ")
-    for i in range(pos_criterion_number):
-        pc = input("\t\t")
-        pos_criterion_name.append(pc)
-
-    neg_criterion_number = int(input("Enter number of negative criterion: "))
-    print("\tEnter " + str(neg_criterion_number) + " criterion name: ")
-    for i in range(neg_criterion_number):
-        nc = input("\t\t")
-        neg_criterion_name.append(nc)
-
-    alternatives.append([["Solution"], pos_criterion_name, neg_criterion_name])
-
-    for i in range(len(alternatives_name)):
-        print("* With " + alternatives_name[i] + ":")
-        lst_pos_para = []
-        lst_neg_para = []
-        for j in range(pos_criterion_number):
-            para = input("\t - " + pos_criterion_name[j] + ": ")
-            lst_pos_para.append(para)
-        for j in range(neg_criterion_number):
-            para = input("\t - " + neg_criterion_name[j] + ": ")
-            lst_neg_para.append(para)
-
-        alternatives.append([[alternatives_name[i]], lst_pos_para, lst_neg_para])
-
+# Formated output alternative
 def print_alternative(number_pos, number_neg, alternatives):
-    row_format = "{:^35} |" + "{:^10} | " * (number_pos + number_neg)
+    row_format = "{:^35} |" + "{:^11} | " * (number_pos + number_neg)
     for alternative in alternatives:
         al = [alternative[i][j] for i in range(len(alternative)) for j in range(len(alternative[i]))]
         print(row_format.format(*al))
@@ -57,26 +31,46 @@ def dominates(alternative1, alternative2):
     
     return True
 
+def pareto_optimal(alternatives):
+    pareto_element = []
+    dominatedAlter = []
+    candidateRowNr = 0
+    tag = alternatives[0]  
+    copy_alternatives = alternatives.copy()
+    copy_alternatives.pop(0)
+
+    while True:
+        candidateRow = copy_alternatives[candidateRowNr]
+        copy_alternatives.remove(candidateRow)
+        rowNr = 0
+        nonDominated = True
+        while len(copy_alternatives) != 0 and rowNr < len(copy_alternatives):
+            row = copy_alternatives[rowNr]
+            if dominates(candidateRow, row):
+                copy_alternatives.remove(row)
+                dominatedAlter.append(row)
+            elif dominates(row, candidateRow):
+                nonDominated = False
+                dominatedAlter.append(candidateRow)
+                rowNr += 1
+            else:
+                rowNr += 1
+        
+        if nonDominated:
+            pareto_element.append(candidateRow)
+
+        if len(copy_alternatives) == 0:
+            break
+    
+    pareto_element.insert(0, tag)
+    dominatedAlter.insert(0, tag)
+    return pareto_element, dominatedAlter
+
 def check_in_pareto(pareto_element, alternative):
     for ele in pareto_element:
         if ele[0][0] == alternative[0][0]:
             return True
     return False
-
-def pareto_optimal(alternatives):
-    pareto_element = []
-    tag = alternatives[0]
-
-    for num_alternative in range(len(alternatives) - 1):
-        copy_alternatives = alternatives.copy()
-        copy_alternatives.pop(0)
-        alternative1 = copy_alternatives.pop(num_alternative)
-    
-        for alternative in copy_alternatives:
-            if dominates(alternative1, alternative) and not check_in_pareto(pareto_element, alternative1):
-                pareto_element.append(alternative1)
-    pareto_element.insert(0, tag)
-    return pareto_element
 
 def f_limit_element(alternatives, NCriterion, limit, sign):
     limit_ele = []
@@ -150,12 +144,14 @@ def find_best_alternative(alternatives, NCriterion):
     tag = copy_alternatives.pop(0)
     
     if NCriterion <= len(pos_criterion_name):
-        NCritetion -= 1
+        NCriterion -= 1
         a_max = copy_alternatives[0]
         for i in range(1, len(copy_alternatives)):
             if float(copy_alternatives[i][1][NCriterion]) > float(a_max[1][NCriterion]):
                 a_max = copy_alternatives[i]
-        best_alternative.append(a_max)
+        for i in range(0, len(copy_alternatives)):
+            if float(copy_alternatives[i][1][NCriterion]) == float(a_max[1][NCriterion]):
+                best_alternative.append(copy_alternatives[i])
 
     elif NCriterion > len(pos_criterion_name):
         NCriterion -= (len(pos_criterion_name) + 1)
@@ -163,7 +159,10 @@ def find_best_alternative(alternatives, NCriterion):
         for i in range(1, len(copy_alternatives)):
             if float(copy_alternatives[i][2][NCriterion]) < float(a_min[2][NCriterion]):
                 a_min = copy_alternatives[i]
-        best_alternative.append(a_min)
+
+        for i in range(0, len(copy_alternatives)):
+            if float(copy_alternatives[i][2][NCriterion]) == float(a_min[2][NCriterion]):
+                best_alternative.append(copy_alternatives[i])
 
     best_alternative.insert(0, tag)
     return best_alternative
@@ -185,7 +184,29 @@ def sub_optimization():
 
     return find_best_alternative(sub_set, choose)
 
-# #input_alternative()
+def lexico_optimization(alternatives):
+    print("Set the order of criterion: ")
+    index = 1
+    for pos_criterion in pos_criterion_name:
+        print("\t" + str(index) + " - " + pos_criterion)
+        index += 1
+    for neg_criterion in neg_criterion_name:
+        print("\t" + str(index) + " - " + neg_criterion)
+        index += 1
+
+    orders = [int(x) for x in input("Enter your order: ").split()]
+
+    lexico_set = alternatives.copy()
+
+    for order in orders:
+        lexico_set = find_best_alternative(lexico_set, order)
+    
+    return lexico_set
+
+
+input_alternative()
+pos_criterion_name = alternatives[0][1]
+neg_criterion_name = alternatives[0][2]
 
 while True:
     print("\n" + "-" * 40 + "MENU" + "-" * 40)
@@ -193,11 +214,12 @@ while True:
     print("[2] - Pareto optimal set")
     print("[3] - Optimal solutions when setting limits")
     print("[4] - Sub-optimization")
+    print("[5] - Lexicographic optimization")
     print("[0] - Exit!")
     print("-" * 40 + "----" + "-" * 40)
     my_choice = int(input("Enter your choice: "))
 
-    pareto_ele = pareto_optimal(alternatives)
+    pareto_set, dominated_set = pareto_optimal(alternatives)
 
     if my_choice == 1:
         print("\n" + "-"*49 + "All alternatives" + "-"*49)
@@ -205,7 +227,7 @@ while True:
 
     elif my_choice == 2:
         print("\n" + "-"*48 + "Pareto Optimal Set" + "-"*48)
-        print_alternative(len(pos_criterion_name), len(neg_criterion_name), pareto_ele)
+        print_alternative(len(pos_criterion_name), len(neg_criterion_name), pareto_set)
 
     elif my_choice == 3:
         print("\n" + "-"*40 + "Specifying limit of criteria" + "-"*40)
@@ -215,7 +237,7 @@ while True:
         print("\n" + "-"*29 + "Set of element when limit has been applied in Pareto set" + "-"*29)
         pareto_limit = []
         for ele in limit_ele:
-            if ele in pareto_ele:
+            if ele in pareto_set:
                 pareto_limit.append(ele)
         print_alternative(len(pos_criterion_name), len(neg_criterion_name), pareto_limit)
 
@@ -224,6 +246,12 @@ while True:
         sub_opt_ele = sub_optimization()
         print("\n" + "-"*41 + "Set of element sub-optimization" + "-"*42)
         print_alternative(len(pos_criterion_name), len(neg_criterion_name), sub_opt_ele)
+
+    elif my_choice == 5:
+        print("\n" + "-"*40 + "Lexicographic optimization" + "-"*40)
+        lexico_set = lexico_optimization(alternatives)
+        print()
+        print_alternative(len(pos_criterion_name), len(neg_criterion_name), lexico_set)
 
     else:
         print("Thank you for using!")
